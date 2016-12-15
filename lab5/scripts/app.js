9bfa8b6f31bbc54c0b7db652951345b189fe5274
dimalert <kirill.sd@mail.ru>
2016-12-15 22:40:50 +0300

    added pushing articles

---
+++ lab5/scripts/app.js
@@ -0,0 +1,45 @@
+'use strict';
+var datat = {};
+var URL = 'https://api.nytimes.com/svc/topstories/v1/travel.json?api-key=7f64edc5d0804574800d7a8b9da058ad';
+var myApp = angular.module('app', [])
+    .controller("MainCtrl", function($scope){
+         window.onload = function() {
+             fetch(URL)  
+                    .then(function(response) {  
+                        if (response.status !== 200) {  
+                            console.log('Looks like there was a problem. Status Code: ' +  
+                            response.status);  
+                            return;  
+                        }
+                    // Examine the text in the response  
+                        response.json().then(function(data) {  
+                              $scope.articles = data.results;
+                              
+                              for(let i = 0; i < $scope.articles.length; i++) {
+                                  if($scope.articles[i].multimedia === "" ) {
+                                      console.log($scope.articles[i].multimedia);
+                                      $scope.articles[i].multimedia = [,,,,{url: "static/cap.jpg"}];
+                                      console.log($scope.articles[i].multimedia);
+                                  }
+                              }
+                              
+                              $scope.$apply();
+                              console.log(data);
+                        });  
+                    }  
+                )  
+                .catch(function(err) {  
+                    console.log('Fetch Error :-S', err);  
+                });
+            
+        }
+    });
+    myApp.controller("FormCtrl", function($scope){
+        $scope.is_articleAdded = false;
+    });
+
+
+
+
+
+    
