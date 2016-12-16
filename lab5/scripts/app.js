a5f82d9e8aa65560e6e175204d4f372ef0936d3e
dimalert <kirilltsibikov@gmail.com>
2016-12-17 00:34:36 +0300

    add service

--- lab5/scripts/app.js
+++ lab5/scripts/app.js
@@ -2,37 +2,42 @@
 var datat = {};
 var URL = 'https://api.nytimes.com/svc/topstories/v1/travel.json?api-key=7f64edc5d0804574800d7a8b9da058ad';
 var myApp = angular.module('app', [])
-    .controller("MainCtrl", function($scope){
-         window.onload = function() {
-             fetch(URL)  
-                    .then(function(response) {  
-                        if (response.status !== 200) {  
-                            console.log('Looks like there was a problem. Status Code: ' +  
-                            response.status);  
-                            return;  
+    .controller("MainCtrl", function($scope, dataService){
+         
+        dataService.getArticles(function(response) {  
+                if (response.status !== 200) {  
+                    console.log('Looks like there was a problem. Status Code: ' +  
+                    response.status);  
+                    return;  
+                }
+                response.json().then(function(data) {  
+                    $scope.articles = data.results;
+                                
+                    for(let i = 0; i < $scope.articles.length; i++) {
+                        if($scope.articles[i].multimedia === "" ) {
+                            console.log($scope.articles[i].multimedia);
+                            $scope.articles[i].multimedia = [,,,,{url: "mock/cap.jpg"}];
+                            console.log($scope.articles[i].multimedia);
                         }
-                    // Examine the text in the response  
-                        response.json().then(function(data) {  
-                              $scope.articles = data.results;
-                              
-                              for(let i = 0; i < $scope.articles.length; i++) {
-                                  if($scope.articles[i].multimedia === "" ) {
-                                      console.log($scope.articles[i].multimedia);
-                                      $scope.articles[i].multimedia = [,,,,{url: "static/cap.jpg"}];
-                                      console.log($scope.articles[i].multimedia);
-                                  }
-                              }
-                              
-                              $scope.$apply();
-                              console.log(data);
-                        });  
-                    }  
-                )  
+                    }
+                        
+                    $scope.$apply();
+                    console.log(data);
+                });  
+            }  
+        );
+
+    });
+    myApp.service("dataService", function() {
+
+        this.getArticles = function(callback) {
+             fetch(URL)  
+                .then(callback)  
                 .catch(function(err) {  
                     console.log('Fetch Error :-S', err);  
                 });
-            
         }
+
     });
     myApp.controller("FormCtrl", function($scope){
         $scope.is_articleAdded = false;
@@ -40,7 +45,7 @@ var myApp = angular.module('app', [])
         function Article() {
             this.title = "";
             this.url = "";
-            this.multimedia = [,,,,{url: "static/cap.jpg"}];
+            this.multimedia = [,,,,{url: "mock/cap.jpg"}];
             this.abstract = "";
         }
         
